cca92eb7fba1029748ad7ec8dc298ca3ba2f1e15
Dan Carreon <daniel.carreong@gmail.com>
2021-03-01 22:54:54 -0600

    Adding solution to FrogRiverOne

---
+++ src/codility/lessons/FrogRiverOne.java
@@ -0,0 +1,27 @@
+package codility.lessons;
+
+import java.util.*;
+
+public class FrogRiverOne {
+
+    public static int solution(int X, int[] A) {
+
+        Set path = new HashSet();
+
+        for (int i = 0; i < A.length; i++) {
+            if (path.add(A[i]) && path.size() == X) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int array[] = new int[]
+                {1, 3, 1, 3, 2, 1, 3};
+        //{2, 2, 2, 2, 2};
+        //{1, 3, 1, 4, 2, 3, 5, 4};
+        System.out.println(solution(3, array));
+    }
+}
