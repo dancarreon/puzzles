988f8fdd4cd815aa77b3bbdd496ce629d8925804
Dan Carreon <daniel.carreong@gmail.com>
2021-03-02 19:37:09 -0600

    Adding solution to MaxCounters

---
+++ src/codility/lessons/MaxCounters.java
@@ -0,0 +1,50 @@
+package codility.lessons;
+
+import java.util.Arrays;
+
+public class MaxCounters {
+
+    public static int[] solution(int N, int[] A) {
+
+        int[] counters = new int[N];
+        int maxValue = 0;
+        int maxCounter = 0;
+
+        for (int i = 0; i < A.length; i++) {
+
+            int operation = A[i];
+
+            if ((operation >= 1) && (operation <= N)) {
+
+                int position = A[i] - 1;
+
+                if (counters[position] < maxCounter) {
+                    counters[position] = maxCounter + 1;
+                } else {
+                    counters[position]++;
+                }
+
+                if (counters[position] > maxValue) {
+                    maxValue = counters[position];
+                }
+            } else if (operation == (N + 1)) {
+                maxCounter = maxValue;
+            }
+        }
+
+        for (int j = 0; j < N; j++) {
+            if (counters[j] < maxCounter) {
+                counters[j] = maxCounter;
+            }
+        }
+
+        return counters;
+    }
+
+    public static void main(String[] args) {
+        int[] array = //{1, 11, 1, 12, 1, 13, 1, 14, 1, 15, 1, 16, 1, 17, 1, 18, 1, 19, 1, 20};
+                {3, 4, 4, 6, 1, 4, 4};
+        //{2, 1, 1, 2, 1};
+        System.out.println(Arrays.toString(solution(5, array)));
+    }
+}
