682b23474f055723dee88937309067dfa1e10271
Dan Carreon <daniel.carreong@gmail.com>
2021-03-04 00:20:44 -0600

    Adding solution for PermCheck

---
+++ src/codility/lessons/PermCheck.java
@@ -0,0 +1,32 @@
+package codility.lessons;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermCheck {
+
+    public static int solution(int[] A) {
+
+        Arrays.sort(A);
+
+        Set<Integer> uniques = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            if (!uniques.add(A[i])) {
+                return 0;
+            }
+        }
+
+        if (A[A.length - 1] != A.length) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {9, 5, 7, 3, 2, 7, 3, 1, 10, 8};
+        //{4, 3, 1};
+        System.out.println(solution(array));
+    }
+}
