48cfde95985711893ba2de814b00c06cb0f3a615
dancarreon <daniel.carreon@clevertech.biz>
2024-05-06 15:35:01 -0600

    Changes

--- src/codility/lessons/FrogRiverOne.java
+++ src/codility/lessons/FrogRiverOne.java
@@ -17,11 +17,26 @@ public class FrogRiverOne {
         return -1;
     }
 
+    public static int solution2(int X, int[] A) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            set.add(A[i]);
+            if (set.size() == X) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int array[] = new int[]
+        int[] array = new int[]
                 {1, 3, 1, 3, 2, 1, 3};
         //{2, 2, 2, 2, 2};
         //{1, 3, 1, 4, 2, 3, 5, 4};
         System.out.println(solution(3, array));
+        System.out.println(solution2(3, array));
     }
 }
--- src/codility/lessons/PermCheck.java
+++ src/codility/lessons/PermCheck.java
@@ -1,6 +1,7 @@
 package codility.lessons;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,9 +25,29 @@ public class PermCheck {
         return 1;
     }
 
+    // 100 score
+    public static int solution2(int[] A) {
+
+        Arrays.sort(A);
+        Set<Integer> uniques = new HashSet<>();
+
+        for (int num : A) {
+           if (!uniques.add(num)) {
+               return 0;
+           }
+        }
+
+        if (A[A.length - 1] != A.length) {
+            return 0;
+        }
+
+        return 1;
+    }
+
     public static void main(String[] args) {
         int[] array = {9, 5, 7, 3, 2, 7, 3, 1, 10, 8};
         //{4, 3, 1};
-        System.out.println(solution(array));
+        //System.out.println(solution(array));
+        System.out.println(solution2(array));
     }
 }
